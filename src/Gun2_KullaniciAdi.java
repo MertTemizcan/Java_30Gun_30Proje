144fe8b3fb8fa05662a86c25e10ab99056d5bd9e
Mert Temizcan <mert.temizcan17@gmail.com>
2025-11-15 09:19:17 +0300

    Ikinci Gun

---
+++ src/Gun2_KullaniciAdi.java
@@ -0,0 +1,10 @@
+public class Gun2_KullaniciAdi {
+    public static void main(String[] args) {
+        String ad = "Mert"; // Ad değişkenini tanımladık
+        String soyad = "Temizcan"; // Soyad değişkenini tanımladık
+        int dogumYili = 2006; // Doğum yılı değişkenini tanımladık
+        int yas = 2025 - 2006; // Şimdiki zamandan doğum yılını çıkartarak yaş değişkenine ulaştık
+
+        System.out.println("Merhaba " + ad + " " + soyad + " yaşınız:" + yas); // Girilen bilgileri ekrana yazdırdık
+    }
+}
